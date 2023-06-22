af08e170a0d6361ad7ad86bfc1600aefbcbadda6
natalie-kolos <kolos.natasha@gmail.com>
2023-06-22 19:32:33 +0200

    All pages

--- js/common.js
+++ js/common.js
@@ -60,4 +60,23 @@ $(document).ready(function () {
       },
     ],
   });
+  $(".related").slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
 });
